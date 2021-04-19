586a151e60cbc65a686eccfbbceb07d84cffb72a
Carlos G. Godri <cggodri@gmail.com>
2021-04-19 15:37:17 -0300

    create project devquiz

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:dev_quiz/pages/home/widgets/app_bar/main.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
---
+++ lib/pages/splash/splash.dart
@@ -0,0 +1,19 @@
+import 'package:dev_quiz/core/app_images.dart';
+import 'package:dev_quiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
+  }
+}
