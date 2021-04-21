db800fc503af609a566d46c17f53b54474499976
Carlos Godri <cggodri@gmail.com>
2021-04-21 16:17:35 -0300

    create quiz_card

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/pages/home/widgets/app_bar/main.dart';
 import 'package:dev_quiz/pages/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/pages/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,9 +13,51 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: LevelButtonWidget(
-        label: 'Fácil',
-      ),
+      body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Wrap(
+                direction: Axis.vertical,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      LevelButtonWidget(
+                        label: 'Fácil',
+                      ),
+                      LevelButtonWidget(
+                        label: 'Médio',
+                      ),
+                      LevelButtonWidget(
+                        label: 'Difícil',
+                      ),
+                      LevelButtonWidget(
+                        label: 'Perito',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                  child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ))
+            ],
+          )),
     );
   }
 }
